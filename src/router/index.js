0537857cc88e751953ef45341e1868a07aa86f58
luowendi <luowendi@umier.com.cn>
2017-11-22 17:40:27 +0800

    实现路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
-import UserCenter from 'components/user-center/user-center'
-import SingerDetail from 'components/singer-detail/singer-detail'
 
 Vue.use(Router)
+
+const Recommend = () => import('components/recommend/recommend')
+const Singer = () => import('components/singer/singer')
+const Rank = () => import('components/rank/rank')
+const Search = () => import('components/search/search')
+const UserCenter = () => import('components/user-center/user-center')
+const SingerDetail = () => import('components/singer-detail/singer-detail')
 
 export default new Router({
   routes: [
