528311ee0eb9b056aadd9c191124454e15ff47a6
luowendi <luowendi@umier.com.cn>
2017-12-09 17:38:48 +0800

    完成歌曲列表基本操作

--- src/components/play-list/play-list.vue
+++ src/components/play-list/play-list.vue
@@ -1,16 +1,41 @@
 <template>
   <transition name="fade">
     <div class="play-list" @click="hide" v-show="showFlag">
-      <div class="play-list-wrapper border-1px-top">
-        <div class="top"></div>
-        <div class="list"></div>
-        <div class="close"></div>
+      <div class="play-list-wrapper border-1px-top" @click.stop>
+        <div class="top">
+          <i class="icon" :class="iconMode"></i>
+          <span class="text">{{modeText}}</span>
+          <span class="clear" @click="clearPlaylist">
+            <i class="icon-clear"></i>
+          </span>
+        </div>
+        <scroll :data="sequenceList" class="list" ref="scroll">
+          <transition-group name="list" tag="ul" ref="list">
+            <li :key="item.id" @click="selectItem(item)" class="item" v-for="item in sequenceList">
+              <i class="current" :class="getCurrentCls(item)"></i>
+              <span class="text" v-html="item.name"></span>
+              <span class="favourite">
+                <i class="icon-not-favorite"></i>
+              </span>
+              <span class="delete" @click.stop="deleteItem(item)">
+                <i class="icon-delete"></i>
+              </span>
+            </li>
+          </transition-group>
+        </scroll>
+        <div class="close" @click="hide">
+          <span class="text">关闭</span>
+        </div>
       </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  import {playMode} from 'common/js/config'
+  import {mapGetters, mapActions, mapMutations} from 'vuex'
+
   export default {
     data() {
       return {
@@ -20,10 +45,75 @@
     methods: {
       show() {
         this.showFlag = true
+        this.$nextTick(() => {
+          this.$refs.scroll.refresh()
+          this.scrollToCurrentSong(this.currentSong)
+        })
       },
       hide() {
         this.showFlag = false
+      },
+      getCurrentCls(item) {
+        if (item.id === this.currentSong.id) {
+          return 'icon-play'
+        }
+        return ''
+      },
+      selectItem(item) {
+        let index = this.sequenceList.findIndex((song) => {
+          return song.id === item.id
+        })
+        this.setCurrentIndex(index)
+      },
+      deleteItem(item) {
+        if (this.isDeleting) {
+          return
+        }
+        this.isDeleting = true
+        this.deleteSong(item)
+        setTimeout(() => {
+          this.isDeleting = false
+        }, 300)
+      },
+      scrollToCurrentSong(song) {
+        let index = this.sequenceList.findIndex((item) => {
+          return song.id === item.id
+        })
+        this.$refs.scroll.scrollToElement(this.$refs.list.$el.children[index], 300)
+      },
+      ...mapMutations({
+        setCurrentIndex: 'SET_CURRENT_INDEX'
+      }),
+      ...mapActions([
+        'deleteSong',
+        'clearPlaylist'
+      ])
+    },
+    watch: {
+      currentSong(newSong, oldSong) {
+        if (newSong.id === oldSong.id) {
+          return
+        }
+        setTimeout(() => {
+          this.scrollToCurrentSong(newSong)
+        }, 30)
       }
+    },
+    computed: {
+      modeText() {
+        return this.mode === playMode.sequence ? '顺序播放' : this.mode === playMode.loop ? '循环播放' : '随机播放'
+      },
+      iconMode() {
+        return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
+      },
+      ...mapGetters([
+        'sequenceList',
+        'currentSong',
+        'mode'
+      ])
+    },
+    components: {
+      Scroll
     }
   }
 </script>
--- src/store/actions.js
+++ src/store/actions.js
@@ -84,3 +84,35 @@ export function deleteSearchHistory({commit}, query) {
 export function clearSearchHistory({commit}) {
   commit(types.SET_SEARCH_HISTORY, clearHistory())
 }
+
+export function deleteSong({commit, state}, song) {
+  let playlist = state.playlist.slice()
+  let sequenceList = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+
+  let pIndex = findIndex(playlist, song)
+  playlist.splice(pIndex, 1)
+
+  let sIndex = findIndex(sequenceList, song)
+  sequenceList.splice(sIndex, 1)
+
+  if (currentIndex > pIndex || currentIndex === playlist.length) {
+    currentIndex--
+  }
+
+  commit(types.SET_PLAYLIST, playlist)
+  commit(types.SET_SEQUENCE_LIST, sequenceList)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
+  if (!playlist.length) {
+    commit(types.SET_PLAYING_STATE, false)
+  } else {
+    commit(types.SET_PLAYING_STATE, true)
+  }
+}
+
+export function clearPlaylist({commit}) {
+  commit(types.SET_CURRENT_INDEX, -1)
+  commit(types.SET_PLAYLIST, [])
+  commit(types.SET_SEQUENCE_LIST, [])
+  commit(types.SET_PLAYING_STATE, false)
+}
